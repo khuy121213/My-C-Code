363e8c68788a1142a885f0a00a50a1f7d1dcf0e2
khuy121213 <vukhachuy1212@icloud.com>
2025-09-07 17:58:39 +0700

    made a new calculator because the last one was useless

---
+++ ActuslCalculator/ActuslCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿//this is to make up for my other "calculator"
+using System.Runtime.InteropServices;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double result = 0;
+        Console.WriteLine("Welcome to the calculator!");
+
+        Console.Write("Choose your first number: ");
+        string stringnum1 = Console.ReadLine();
+
+        if (!double.TryParse(stringnum1, out double num1))
+        {
+            Console.Write("Invalid number for the first number!");
+            return;
+        }
+
+        Console.Write("Choose an operator (+ - * /): ");
+        string Operator = Console.ReadLine();
+
+        Console.Write("Choose your second number: ");
+        string stringnum2 = Console.ReadLine();
+
+        if (!double.TryParse(stringnum2, out double num2))
+        {
+            Console.Write("Invalid number for the second number!");
+            return;
+        }
+
+        if (Operator == "+")
+        {
+            result = num1 + num2;
+        }
+        else if (Operator == "-")
+        {
+            result = num1 - num2;
+        }
+        else if (Operator == "*")
+        {
+            result = num1 * num2;
+        }
+        else if (Operator == "/")
+        {
+            result = num1 / num2;
+        }
+
+        Console.Write($"The result of {num1} {Operator} {num2} is: {result}");
+        Console.WriteLine("Finished calculator!");
+    }
+}
