c7ff5f75e32f4aef5176c5920b3d438d54a6b0f5
khuy121213 <vukhachuy1212@icloud.com>
2025-09-07 13:10:18 +0700

    improved age error handling

--- Introductor/Introductor/Program.cs
+++ Introductor/Introductor/Program.cs
@@ -7,11 +7,19 @@ class Program
         Console.Write("Whats your name: ");
         string name = Console.ReadLine();
         Console.Write("How old are you: ");
-        int age = int.Parse(Console.ReadLine());
-        Console.Write("Whats your favorite food: ");
-        string food = Console.ReadLine();
-        Console.Write("Whats your second favorite food: ");
-        string food2 = Console.ReadLine();
-        Console.WriteLine($"Hi, my name is {name}!I am {age} years old!I like {food} and {food2}!");
+        string ageString = Console.ReadLine();
+        if(int.TryParse(ageString , out int age))
+        {
+            Console.Write("Whats your favorite food: ");
+            string food = Console.ReadLine();
+            Console.Write("Whats your second favorite food: ");
+            string food2 = Console.ReadLine();
+            Console.WriteLine($"Hi, my name is {name}! I am {age} years old! I like {food} and {food2}!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid age number!");
+        }
+        
     }
 }
