e115ee75832fce331bfd672f1dfcf72209d66cc7
khuy121213 <vukhachuy1212@icloud.com>
2025-09-07 18:28:18 +0700

    created new unit converter:added menu demo

---
+++ Unit Converter/Unit Converter/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to my basic unit converter!");
+
+        Console.WriteLine("Choose what do you wanna convert: Kilometers to Miles\nMiles to Kilometers\nMeters to Feet\nFeet to Meters\nWeight/Mass\nKilograms to Pounds\nPounds to Kilograms\nTemperature\nCelsius to Fahrenheit\nFahrenheit to Celsius");
+    }
+}
