206636bb7cce4e6800e39f147fab00086cb8c8f1
khuy121213 <vukhachuy1212@icloud.com>
2025-09-13 16:03:13 +0700

    Made a new incremental game(big project)

---
+++ IdleIncremental/IdleIncremental/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace IdleIncrement;
+public class MainGame
+{
+
+    public static void Main()
+    {
+        int currencyCD = 1000;
+        Currency currency = new Currency();
+        while (true)
+        {
+            currency.Coins += currency.CoinsPS;
+            Thread.Sleep(currencyCD);
+            Console.WriteLine($"\rYou have {currency.Coins} coins");
+            if (currency.Coins == 10)
+            {
+                break;
+            }
+        }
+    }
+
+}
+
+public class Currency
+{
+    public double Coins { get; set; } = 0;
+    public double CoinsPS { get; set; } = 1;
+}
