5603644348a9f3ef5f701da8d37bf2ddafa2edab
khuy121213 <vukhachuy1212@icloud.com>
2025-09-13 19:45:48 +0700

    added an upgrade button and a goal 13/09/25 v1.00.00

--- IdleIncremental/IdleIncremental/Program.cs
+++ IdleIncremental/IdleIncremental/Program.cs
@@ -1,28 +1,57 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 namespace IdleIncrement;
 public class MainGame
 {
 
     public static void Main()
     {
-        int currencyCD = 1000;
-        Currency currency = new Currency();
+        int baseCd = 1000;
+        int currentCd = baseCd;
+        var state = new GameState();
+        Console.WriteLine("Press E to upgrade speed");
         while (true)
         {
-            currency.Coins += currency.CoinsPS;
-            Thread.Sleep(currencyCD);
-            Console.WriteLine($"\rYou have {currency.Coins} coins");
-            if (currency.Coins == 10)
+            state.Coins += state.CoinsPS;
+            Thread.Sleep(currentCd);
+            Console.Write("\r" + new string(' ', 50)); // clear the line
+            Console.Write($"\rYou have {state.Coins} coins");
+            if (Console.KeyAvailable)
+            {
+                var keyInfo = Console.ReadKey(true);
+                char key = char.ToLower(keyInfo.KeyChar);
+                if(key == 'e')
+                {
+                    Upgrades.UpgradeSpeed(state);
+                }
+            }
+            if (state.Coins == 1000)
             {
                 break;
             }
         }
+        }
     }
 
-}
-
-public class Currency
+public class GameState
 {
+    public int currentCd = 1000;
     public double Coins { get; set; } = 0;
     public double CoinsPS { get; set; } = 1;
+}
+
+public class Upgrades
+{
+    public static void UpgradeSpeed(GameState state)
+    {
+        if (state.Coins >= 10)
+        {
+            state.currentCd = Math.Max(100, state.currentCd - 50);
+            Console.WriteLine();
+            Console.WriteLine($"You upgraded!Your new cooldown is {state.currentCd}");
+            Thread.Sleep(500);
+            Console.Write("\r" + new string(' ', 50));
+        }
+    }
 }
