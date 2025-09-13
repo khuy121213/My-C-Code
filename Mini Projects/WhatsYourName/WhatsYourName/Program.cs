1ce0066cde1be3c12fbdcabce085218b9d66d88a
khuy121213 <vukhachuy1212@icloud.com>
2025-09-13 14:45:13 +0700

    improved program

---
+++ Mini Projects/WhatsYourName/WhatsYourName/Program.cs
@@ -0,0 +1,28 @@
+﻿while (true)
+{
+    Console.Write("Whats your name: ");
+    string name = Console.ReadLine();
+
+    if (name == "bill" || name == "BILL" || name == "Bill")
+    {
+        Console.WriteLine("DID U JUST SAY 67????");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("BOII");
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("SIX ");
+            Console.WriteLine("SEVEN ");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Hello {name}!");
+    }
+    Console.WriteLine();
+    Console.Write("Enter a key to restart: ");
+    Console.ReadKey(true);
+    Console.Clear();
+    continue;
+}
