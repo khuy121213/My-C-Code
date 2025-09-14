45857c9c0721db69eb419dc0ff7d8455184b37dd
khuy121213 <vukhachuy1212@icloud.com>
2025-09-14 19:00:51 +0700

    v1.04.00 : Made a new goal, MP and MP upgrades and small improvements

--- IdleIncremental/IdleIncremental/Program.cs
+++ IdleIncremental/IdleIncremental/Program.cs
@@ -10,22 +10,46 @@ public class MainGame
     {
         var state = new GameState();
         Console.CursorVisible = false;
+        Console.Clear(); // Fix the capitalization
         int speedCost = 10;
         int cpsCost = 20;
-        Console.WriteLine("Press E to upgrade speed , F to upgrade coin/s per sec");
+        int mpCost = 1; // Add mpCost variable
         string lastMessage = "";
         int messageTime = 0;
         int lastCointick = Environment.TickCount;
+        Console.WriteLine("Hello and welcome to my game, here is the tutorial: \nYou have a coin counter and a coin per sec counter, \nyou can upgrade them by pressing E for speed and F for CPS. \nThe goal is to get 1 billion coins to pay off your very bad financial debt. \nGood luck!");
+
+        Console.Write("Press any key to start the game!:");
+        Console.ReadKey(true);
+        for (int i = 0; i < 7; i++)
+        {
+            Console.SetCursorPosition(0, i);
+            Console.Write(new string(' ', Console.WindowWidth));
+        }
+
+        Console.WriteLine("Press E to upgrade speed , F to upgrade coin/s per sec , P to get MP , M for MP upgrades.");
         while (true)
         {
-            Console.SetCursorPosition(0, 2);
+            Console.SetCursorPosition(0, 1);
             Console.Write("\r" + new string(' ', 50)); // clear the line
-            Console.SetCursorPosition(0, 2);
+            Console.SetCursorPosition(0, 1);
             Console.Write($"\rCoins: {state.Coins}");
+
             Console.SetCursorPosition(0, 4);
             Console.Write("\r" + new string(' ', 50));  // clear line
             Console.SetCursorPosition(0, 4);
-            Console.Write("Prices: Speed: " + speedCost + " , CPS: " + cpsCost);
+            Console.Write($"\r Stats: Speed: {state.currentCd} ms , CPS: {state.CoinsPS} coin per sec");
+
+            Console.SetCursorPosition(0, 3);
+            Console.Write("\r" + new string(' ', 50));  // clear line
+            Console.SetCursorPosition(0, 3);
+            Console.Write("Prices: Speed: " + speedCost + " , CPS: " + cpsCost + " , MP Upgrade: 1");
+
+            Console.SetCursorPosition(0, 5);
+            Console.Write("\r" + new string(' ', 50));  // clear line
+            Console.SetCursorPosition(0, 5);
+            Console.Write($"MP: {state.MP}");
+
             if (Console.KeyAvailable)
             {
                 var keyInfo = Console.ReadKey(true);
@@ -41,6 +65,42 @@ public class MainGame
                     lastMessage = Upgrades.UpgradeCPS(state , ref cpsCost);
                     messageTime = Environment.TickCount;
                 }
+                else if (key == 'p')
+                {
+                    if(state.Coins >= 1000)
+                    {
+                        state.Coins -= 1000;
+                        state.MP += 1;
+                        Console.WriteLine($"You got {state.MP} MP!");
+                        Console.WriteLine("Your stats and upgrades will be reset"); // Add missing semicolon
+                        state.CoinsPS = 1;
+                        state.currentCd = 1000;
+                        state.Coins = 0;
+                        // Reset upgrade costs as well
+                        speedCost = 10;
+                        cpsCost = 20;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You dont have enough coins!");
+                    }
+                }
+                else if(key == 'm')
+                {
+                    if(state.MP >= 1 && state.MPUpgradeIsPurchased == false)
+                    {
+                        lastMessage = Upgrades.UpgradeMPSpeed(state , ref mpCost);
+                        messageTime = Environment.TickCount;
+                    }
+                    else if(state.MP >= 1 && state.MPUpgradeIsPurchased == true)
+                    {
+                        Console.WriteLine("You have alrady purchased this upgrade!You can only buy one of each MP upgrade"); // Add missing semicolon
+                    }
+                    else
+                    {
+                        Console.WriteLine("You dont have enough MP!");
+                    }
+                }
                     while (Console.KeyAvailable)
                     {
                         Console.ReadKey(true); // read and discard everything left
@@ -51,18 +111,33 @@ public class MainGame
                 state.Coins += state.CoinsPS;
                 lastCointick = Environment.TickCount;
             }
-            Console.SetCursorPosition(0, 3);
+            Console.SetCursorPosition(0, 2);
             Console.Write("\r" + new string(' ', 50));
             int currentTime = Environment.TickCount;
             if ( currentTime - messageTime < 1000)
             {
-                Console.SetCursorPosition(0, 3);
+                Console.SetCursorPosition(0, 2);
                 Console.Write(lastMessage);
             }
             Thread.Sleep(10);
-            if (state.Coins >= 1000)
+
+            
+
+            if (state.Coins >= 1000000000)
+
             {
-                break;
+                Console.WriteLine("You won!");
+                Console.WriteLine("Press R to restart or any other key to exit");
+                var input = Console.ReadKey();
+                if(char.ToLower(input.KeyChar) == 'r')
+                {
+                    Main();
+                }
+                else
+                {
+                    break;
+                }
+                    
             }
         }
         }
@@ -73,6 +148,8 @@ public class GameState
     public int currentCd = 1000;
     public int Coins { get; set; } = 0;
     public int CoinsPS { get; set; } = 1;
+    public int MP { get; set; } = 0;
+    public bool MPUpgradeIsPurchased { get; set; } = false;
 }
 
 public class Upgrades
@@ -109,4 +186,22 @@ public class Upgrades
             return "You dont have enough coins!";
         }
     }
+
+    public static string UpgradeMPSpeed(GameState state , ref int mpCost)
+    {
+        if (state.MP >= mpCost)
+        {
+            state.MP -= mpCost;
+            state.CoinsPS += 4;
+            state.MPUpgradeIsPurchased = true;
+            Console.WriteLine();
+            return $"You upgraded!Your new CPS is {state.CoinsPS} per sec";
+        }
+        else
+        {
+            return "You dont have enough coins!";
+        }
+    }
+
 }
+//Made by Khachuy
